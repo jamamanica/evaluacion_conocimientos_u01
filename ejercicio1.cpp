5486058417a38b73d8f59437ab52f48afc6fddd6
jamamanica <jamamanica>
2024-06-23 11:55:39 -0500

    Actualiza ejercicio 1: agrega validaciones

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,25 +1,36 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int op1,op2,r;
+    int op1,op2;
+    float r;
     char op;
+    cout<<"<<CALCULADORA>>"<<endl;
     cout<<"Primer operando: "; cin>>op1;
     cout<<"Segundo operando: "; cin>>op2;
     cout<<"Operador: "; cin>>op;
     switch(op){
         case '+':
         r=op1+op2;
+        cout<<"Resultado: "<<r;
         break;
         case '-':
         r=op1-op2;
+        cout<<"Resultado: "<<r;
         break;
         case '*':
         r=op1*op2;
+        cout<<"Resultado: "<<r;
         break;
         case '/':
-        r=op1/op2;
+        if(op2!=0){
+            r=op1/op2;
+            cout<<"Resultado: "<<r;
+        } else
+            cout<<"Indeterminado";
+        break;
+        default:
+            cout<<"ERROR. Operador desconocido";
         break;
     }
-    cout<<"Resultado: "<<r<<endl;
     return 0;
 }
