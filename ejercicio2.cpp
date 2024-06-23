1d463eaa15ffed32ca9c92e2ac57f738f741e5a5
jamamanica <jamamanica>
2024-06-23 11:58:45 -0500

    Actualiza ejercicio 2: optimizacion de codigo

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,17 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n1,n2,c,i;
+    int n1,n2;
+    cout<<"<<PESI>>"<<endl;
     cout<<"Primer numero: "; cin>>n1;
     cout<<"Segundo numero: "; cin>>n2;
-    c=0;
-    i=1;
-    while(i<=n1&&i<=n2){
+    int c=0;
+    for(int i=1;i<=n1&&i<=n2;i++){
         if(n1%i==0&&n2%i==0){
             c++;
-            i++;
-        } else 
-            i++;
+        }
     }
     if(c>1){
         cout<<"No son PESI";
